8f378dbacca1f325b9b9df22188f4f5aec24cda5
rainer.telesko <rainer.telesko@fhnw.ch>
2020-02-17 15:21:53 +0100

    Second commit

--- app/src/main/java/com/example/lotterybuttonthree/MainActivity.java
+++ app/src/main/java/com/example/lotterybuttonthree/MainActivity.java
@@ -11,28 +11,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     // GUI controls
     private TextView tvText;
-    private Button btGenerateText;
+    private Button btGenerateText, btGenerateTextTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Getting references to the Button and the TextView
+        // Getting references to the two Buttons and the TextView
         btGenerateText = findViewById(R.id.btGenerateText);
+        btGenerateTextTwo = findViewById(R.id.btGenerateTextTwo);
         tvText = findViewById(R.id.tvText);
 
-        // Setting Listener to Button
+        // Setting Listener to both Buttons
         btGenerateText.setOnClickListener(this);
+        btGenerateTextTwo.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btGenerateText:
-                tvText.setText("Let's go for some tips!");
+                tvText.setText("Let's go for some great Swisslos tips!");
+                break;
+            case R.id.btGenerateTextTwo:
+                tvText.setText("Let's go for some great Euro Millions tips!");
         }
     }
-
-
 }
